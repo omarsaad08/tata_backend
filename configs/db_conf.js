babfc786719f9f104838afa45c0605d046705734
Omar Saad <omarsaad08@outlook.com>
2024-11-15 18:28:58 +0200

    yarab pls ToT

--- configs/db_conf.js
+++ configs/db_conf.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',
-  host: '172.31.37.231',
+  host: 'tata.cfk8wq8e8bzc.eu-north-1.rds.amazonaws.com',
   database: 'tata',
   password: '411444291',
   port: 5432,
